b6ab542652479dd74414caddf4ab5161d8affde4
Carlos Zanella <cefzanella@gmail.com>
2025-08-01 13:55:47 -0300

    feat: reep thread in memory during cooking

--- version-a/src/app/recipe/[recipeId]/cook/page.tsx
+++ version-a/src/app/recipe/[recipeId]/cook/page.tsx
@@ -2,8 +2,9 @@
 import { useGetRecipe } from "@/hooks/useGetRecipe";
 import { useSpeechToText } from "@/hooks/useSpeechToText";
 import { useVad } from "@/hooks/useVad";
+import { AgentInputItem } from "@openai/agents";
 import { useParams } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import styles from './cook.module.css';
 
 type CookProps = {
@@ -11,16 +12,20 @@ type CookProps = {
 };
 
 export default function Cook() {
+  const [thread, setThread] = useState<AgentInputItem[]>([]);
+
   const { recipeId } = useParams<CookProps>();
+  const { data: recipe, isLoading, error } = useGetRecipe(parseInt(recipeId))
+
   const { trigger: triggerSpeechToText } = useSpeechToText();
   const onRecord = useCallback(
     (audio: Float32Array<ArrayBufferLike>) => {
-      triggerSpeechToText(audio).then(r => console.log('Hey!', r));
+      triggerSpeechToText(audio).then(content => {
+        setThread(thread => [...thread, { role: 'user', content }]);
+      });
     },
     [triggerSpeechToText],
-  )
-
-  const { data: recipe, isLoading, error } = useGetRecipe(parseInt(recipeId))
+  );
   const recording = useVad({ onRecord, active: Boolean(recipe) });
 
   if (error) {
@@ -45,6 +50,7 @@ export default function Cook() {
         🎙️ Voice Cooking Assistant {recording ? '- Recording...' : null}
       </header>
       <section>
+        {thread.map((t, i) => <p key={i}>{JSON.stringify(t)}</p>)}
       </section>
     </>
   );
